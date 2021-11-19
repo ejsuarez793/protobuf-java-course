001b85a19dca8d48a46a3b84492656ad325032bc
Enrique Suarez <enriquesuarez@Enriques-MacBook-Pro.local>
2021-11-18 22:36:43 -0300

    27 - Simple Message Creation in Java

--- src/main/java/com/github/ejsuarez793/protobuf/SimpleMain.java
+++ src/main/java/com/github/ejsuarez793/protobuf/SimpleMain.java
@@ -1,7 +1,57 @@
 package com.github.ejsuarez793.protobuf;
 
+import example.simple.Simple.SimpleMessage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class SimpleMain {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        SimpleMessage.Builder builder = SimpleMessage.newBuilder();
+
+        // simple fields
+        builder.setId(42)
+                .setIsSimple(true)
+                .setName("My Simple Message Name");
+
+        // repeated field
+        builder.addSampleList(1)
+                .addSampleList(2)
+                .addSampleList(3)
+                .addAllSampleList(Arrays.asList(4, 5, 6));
+
+        System.out.println(builder.toString());
+
+        SimpleMessage message = builder.build();
+
+        // write the protocol buffers binary to a file
+        try {
+            FileOutputStream outputStream = new FileOutputStream("simple_message.bin");
+            message.writeTo(outputStream);
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // send as byte array
+        byte[] bytes = message.toByteArray();
+
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream("simple_message.bin");
+            SimpleMessage messageFromFile = SimpleMessage.parseFrom(fileInputStream);
+            System.out.println(messageFromFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
