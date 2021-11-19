bc9db9981d5e37984246f778c53fd7a76f1924ba
Enrique Suarez <enriquesuarez@Enriques-MacBook-Pro.local>
2021-11-18 23:04:33 -0300

    30 - Java options

---
+++ src/main/java/com/github/ejsuarez793/protobuf/OptionsMain.java
@@ -0,0 +1,9 @@
+package com.github.ejsuarez793.protobuf;
+
+import com.example.options.OptionMessageOther;
+
+public class OptionsMain {
+    public static void main(String[] args) {
+        OptionMessageOther.Builder other = OptionMessageOther.newBuilder();
+    }
+}
